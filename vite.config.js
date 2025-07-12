0a633c70320cd26f376416a063fee0bfcfcf9251
Rekuiem84 <morinijoshua@gmail.com>
2025-07-12 23:21:39 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import restart from "vite-plugin-restart";
 
 export default {
-	base: "/threejs-journey/",
+	base: process.env.NODE_ENV === "gh-pages" ? "/threejs-journey/" : "/", // Base path for production
 	root: "src/", // Sources files (typically where index.html is)
 	publicDir: "../static/", // Path from "root" to static assets (files that are served as they are)
 	server: {
