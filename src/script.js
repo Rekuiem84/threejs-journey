876b280450a3ff655c05dcea5f6e0b01cf42a80c
Rekuiem84 <morinijoshua@gmail.com>
2025-07-14 00:57:23 +0200

    lights

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
+import { RectAreaLightHelper } from "three/examples/jsm/Addons.js";
 
 /**
  * Base
@@ -17,14 +18,65 @@ const scene = new THREE.Scene();
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 50);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
+gui.add(ambientLight, "intensity").min(0).max(5).step(0.001);
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 1);
+directionalLight.position.set(1, 0.25, 0);
+scene.add(directionalLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 1);
+scene.add(hemisphereLight);
+
+const pointLight = new THREE.PointLight(0xff9000, 1.5, 10, 2);
+pointLight.position.set(0, 0.5, 1);
 scene.add(pointLight);
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 15, 1, 1);
+rectAreaLight.position.set(-1.5, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3());
+scene.add(rectAreaLight);
+
+const spotlight = new THREE.SpotLight(0x78ff00, 4.5, 8, Math.PI * 0.1, 0.2, 1);
+spotlight.position.set(2.5, 1, 1);
+scene.add(spotlight);
+
+// La spotlight regarde dans la direction de sa target,
+// mais cette target n'est pas de base dans la scène
+spotlight.target.position.x = -0.5;
+scene.add(spotlight.target);
+
+// Light Helpers
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(
+	hemisphereLight,
+	0.25
+);
+// scene.add(HemisphereLightHelper);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+	directionalLight,
+	0.25
+);
+// scene.add(directionalLightHelper);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+// scene.add(pointLightHelper);
+
+const spotlightHelper = new THREE.SpotLightHelper(spotlight);
+// scene.add(spotlightHelper);
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
+// scene.add(rectAreaLightHelper);
+
+// scene.add(
+// 	hemisphereLightHelper,
+// 	directionalLightHelper,
+// 	pointLightHelper,
+// 	spotlightHelper,
+// 	rectAreaLightHelper
+// );
 
 /**
  * Objects
@@ -47,7 +99,7 @@ torus.position.x = 1.5;
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
 plane.rotation.x = -Math.PI * 0.5;
-plane.position.y = -0.65;
+plane.position.y = -1;
 
 scene.add(sphere, cube, torus, plane);
 
